c85880c3e70d3035ccb5ed81e3bd80d68026a41f
swigz <daniel.sa.ferreira.7@gmail.com>
2017-04-17 15:03:02 +0100

    Routing now working

--- app.js
+++ app.js
@@ -2,3 +2,34 @@
  * Created by Daniel on 4/10/2017.
  */
 var testeApp = angular.module('testeApp', ['ngRoute','ngAnimate']);
+testeApp.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/about',{
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
+        .when('/links',{
+            templateUrl: 'pages/links.html',
+            controller: 'linksController'
+        })
+        .when('/contact',{
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
+        })
+});
+testeApp.controller('mainController',function($scope){
+    $scope.message="PeidoHome";
+});
+testeApp.controller('aboutController',function($scope){
+    $scope.message="PeidoAbout"
+})
+testeApp.controller('linksController',function($scope){
+    $scope.message="PeidoLinks"
+})
+testeApp.controller('contactController',function($scope){
+    $scope.message="PeidoContact"
+})
